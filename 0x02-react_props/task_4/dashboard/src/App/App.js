995980035f99d331a100730ca3378e216340b51f
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-09-06 00:59:20 +0300

    Update App.js

--- 0x02-react_props/task_4/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -6,22 +6,28 @@ import Notifications from "./Notifications";
 import Header from "./Header";
 import Login from "./Login";
 import Footer from "./Footer";
+import PropTypes from "prop-types";
+import CourseList from "../CourseList/CourseList";
 
-function App() {
-   return (
+function App(isLoggedIn) {
+  return (
     <React.Fragment>
       <Notifications />
       <div className="App">
         <Header />
-        <div className="App-body">
-          <Login />
-        </div>
-        <div className="App-footer">
-          <Footer />
-        </div>
+        {isLoggedIn ? <CourseList /> : <Login />}
+        <Footer />
       </div>
     </React.Fragment>
   );
 }
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
 
 export default App;
