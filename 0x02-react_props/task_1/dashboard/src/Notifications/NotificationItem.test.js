d84b1c7bb6b5ab05cb9a84651218ea551a5a2b21
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-09-05 16:05:55 +0300

    Update NotificationItem.test.js

--- 0x02-react_props/task_1/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x02-react_props/task_1/dashboard/src/Notifications/NotificationItem.test.js
@@ -1,38 +1,38 @@
-import React from "react";
-import { shallow } from "enzyme";
-import NotificationItem from "./NotificationItem";
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
 
-describe("NotificationItem component tests", () => {
-  it("renders NotificationItem component without crashing", () => {
+describe('NotificationItem component tests', () => {
+  it('renders NotificationItem component without crashing', () => {
     const notificationItem = shallow(<NotificationItem />);
-    expect(notificationItem).toBeDefined();
+    expect(notificationItem.exists()).toBe(true);
   });
 
-  it("renders a <li> tag with data-notification-type attribute", () => {
-    const type = "default";
-    const value = "test";
-    const html = { __html: '<u>test</u>' };
-    
-    const notificationItem = shallow(
-      <NotificationItem type={type} value={value} html={html} />
-    );
+  it('renders correct HTML when type and value props are provided', () => {
+    const type = 'default';
+    const value = 'test';
 
-    const liElement = notificationItem.find("li");
-    expect(liElement).toHaveLength(1);
-    expect(liElement.prop("data-notification-type")).toBe(type);
+    const notificationItem = shallow(<NotificationItem type={type} value={value} />);
+
+    const liElement = notificationItem.find('li');
+    expect(liElement.exists()).toBe(true);
+    expect(liElement.prop('data-notification-type')).toBe(type);
+
+    const spanElement = notificationItem.find('span');
+    expect(spanElement.exists()).toBe(true);
+    expect(spanElement.text()).toBe(value);
   });
 
-  it("renders correct HTML content using dangerouslySetInnerHTML", () => {
-    const type = "default";
-    const value = "test";
+  it('renders correct HTML when html prop is provided', () => {
     const html = { __html: '<u>test</u>' };
-    
-    const notificationItem = shallow(
-      <NotificationItem type={type} value={value} html={html} />
-    );
 
-    const spanElement = notificationItem.find("span");
-    expect(spanElement).toHaveLength(1);
-    expect(spanElement.prop("dangerouslySetInnerHTML")).toEqual(html);
+    const notificationItem = shallow(<NotificationItem html={html} />);
+
+    const liElement = notificationItem.find('li');
+    expect(liElement.exists()).toBe(true);
+
+    const spanElement = notificationItem.find('span');
+    expect(spanElement.exists()).toBe(true);
+    expect(spanElement.prop('dangerouslySetInnerHTML')).toEqual(html);
   });
 });
