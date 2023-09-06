f1bcf0c293bc77f560f4c343d502b805d73aad69
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-09-06 15:03:59 +0300

    Shapes, Loops, and Keys

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -10,6 +10,13 @@ import PropTypes from "prop-types";
 import CourseList from "../CourseList/CourseList";
 
 function App(isLoggedIn) {
+   // Create the list of courses
+  const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
+  ];
+  
   return (
     <React.Fragment>
       <Notifications />
