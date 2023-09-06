26c7eb13e46306fce18295acc87f3b1cf657f571
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-09-06 16:02:08 +0300

    Shapes, Loops, and Keys

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -16,7 +16,13 @@ function App(isLoggedIn) {
     { id: 2, name: 'Webpack', credit: 20 },
     { id: 3, name: 'React', credit: 40 },
   ];
-  
+
+   const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: getLatestNotification() },
+];
+   
   return (
     <React.Fragment>
       <Notifications />
